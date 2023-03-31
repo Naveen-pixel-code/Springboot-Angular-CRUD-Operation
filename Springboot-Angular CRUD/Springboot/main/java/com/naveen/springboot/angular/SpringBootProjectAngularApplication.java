4e0ab7816a83d3b21bf07f64391260021bc2bf0d
Naveen-pixel-code <70531012+Naveen-pixel-code@users.noreply.github.com>
2023-03-31 08:29:00 +0530

    Add files via upload

---
+++ Springboot-Angular CRUD/Springboot/main/java/com/naveen/springboot/angular/SpringBootProjectAngularApplication.java
@@ -0,0 +1,13 @@
+package com.naveen.springboot.angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectAngularApplication.class, args);
+	}
+
+}
---
+++ Springboot-Angular CRUD/Springboot/main/java/com/naveen/springboot/angular/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.naveen.springboot.angular.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.naveen.springboot.angular.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+	
+	
+
+}
